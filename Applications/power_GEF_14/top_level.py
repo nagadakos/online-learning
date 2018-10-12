52e912a31afbe51f6be7277ea662931237b49fc5
Nikolaos Agadakos <nagadakos@gmail,com>
2018-10-12 12:41:11 -0400

    Integrated custom solvers module:wq

--- Applications/power_GEF_14/top_level.py
+++ Applications/power_GEF_14/top_level.py
@@ -1,4 +1,9 @@
 import sys as sys
 
+# WARNING:  This is relevant to the directory that CALLS this toplevel
+# Module, rather than the position of this module.
+sys.path.insert(0, './Code/')
 
-printf("Hello from power_GEF_14!")
+from  Solvers import sgd
+
+print("Hello from power_GEF_14!")
--- Code/Solvers/sgd.py
+++ Code/Solvers/sgd.py
@@ -1,56 +1,56 @@
 import torch
-from .optimizer import Optimizer, required
+from torch.optim import Optimizer
 
 
-[docs]class SGD(Optimizer):
-    r"""Implements stochastic gradient descent (optionally with momentum).
+class SGD(Optimizer):
+    # """Implements stochastic gradient descent (optionally with momentum).
 
-    Nesterov momentum is based on the formula from
-    `On the importance of initialization and momentum in deep learning`__.
+    # Nesterov momentum is based on the formula from
+    # `On the importance of initialization and momentum in deep learning`__.
 
-    Args:
-        params (iterable): iterable of parameters to optimize or dicts defining
-            parameter groups
-        lr (float): learning rate
-        momentum (float, optional): momentum factor (default: 0)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
-        dampening (float, optional): dampening for momentum (default: 0)
-        nesterov (bool, optional): enables Nesterov momentum (default: False)
+    # Args:
+        # params (iterable): iterable of parameters to optimize or dicts defining
+            # parameter groups
+        # lr (float): learning rate
+        # momentum (float, optional): momentum factor (default: 0)
+        # weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        # dampening (float, optional): dampening for momentum (default: 0)
+        # nesterov (bool, optional): enables Nesterov momentum (default: False)
 
-    Example:
-        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
-        >>> optimizer.zero_grad()
-        >>> loss_fn(model(input), target).backward()
-        >>> optimizer.step()
+    # Example:
+        # >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+        # >>> optimizer.zero_grad()
+        # >>> loss_fn(model(input), target).backward()
+        # >>> optimizer.step()
 
-    __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
+    # __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
 
-    .. note::
-        The implementation of SGD with Momentum/Nesterov subtly differs from
-        Sutskever et. al. and implementations in some other frameworks.
+    # .. note::
+        # The implementation of SGD with Momentum/Nesterov subtly differs from
+        # Sutskever et. al. and implementations in some other frameworks.
 
-        Considering the specific case of Momentum, the update can be written as
+        # Considering the specific case of Momentum, the update can be written as
 
-        .. math::
-                  v = \rho * v + g \\
-                  p = p - lr * v
+        # .. math::
+                  # v = \rho * v + g \\
+                  # p = p - lr * v
 
-        where p, g, v and :math:`\rho` denote the parameters, gradient,
-        velocity, and momentum respectively.
+        # where p, g, v and :math:`\rho` denote the parameters, gradient,
+        # velocity, and momentum respectively.
 
-        This is in contrast to Sutskever et. al. and
-        other frameworks which employ an update of the form
+        # This is in contrast to Sutskever et. al. and
+        # other frameworks which employ an update of the form
 
-        .. math::
-             v = \rho * v + lr * g \\
-             p = p - v
+        # .. math::
+             # v = \rho * v + lr * g \\
+             # p = p - v
 
-        The Nesterov version is analogously modified.
-    """
+        # The Nesterov version is analogously modified.
+    # """
 
-    def __init__(self, params, lr=required, momentum=0, dampening=0,
+    def __init__(self, params, lr=0.001, momentum=0, dampening=0,
                  weight_decay=0, nesterov=False):
-        if lr is not required and lr < 0.0:
+        if  lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if momentum < 0.0:
             raise ValueError("Invalid momentum value: {}".format(momentum))
@@ -68,13 +68,13 @@ from .optimizer import Optimizer, required
         for group in self.param_groups:
             group.setdefault('nesterov', False)
 
-[docs]    def step(self, closure=None):
-        """Performs a single optimization step.
+    def step(self, closure=None):
+        # """Performs a single optimization step.
 
-        Arguments:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
-        """
+        # Arguments:
+            # closure (callable, optional): A closure that reevaluates the model
+                # and returns the loss.
+        # """
         loss = None
         if closure is not None:
             loss = closure()
