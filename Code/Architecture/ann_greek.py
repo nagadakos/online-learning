3508e6d7e398d2bc7c98adcfb38a0e0d4cf58f43
Nikolaos Agadakos <nagadakos@gmail,com>
2018-10-22 14:51:25 -0400

    Added a new ANN Architecture as ANN-GREEK

---
+++ Code/Architecture/ann_greek.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optm
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os 
+
+
+# Add this files directory location, so we can include the index definitions
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# add this so we can import the trainer and tester modules
+tools_path = os.path.join(dir_path, "../")
+sys.path.insert(0, tools_path)
+
+from Tools import trainer, tester
+import regression_idx as ridx
+
+
+# End Of imports -----------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
+
+sign = lambda x: ('+', '')[x < 0]
+
+class ANNSTLFGREEK(nn.Module):
+    '''
+        Simple single layer feedforwards network, for load preditction.
+        It is found in Hond Tao's PhD dissertation as the entry level
+        articificial neural netowork architecture.
+
+        Inputs:     (u int) Hourly Linear Trend: Each hour has its owh index, 
+                            on the data set. Starting from a base date, each 
+                            hour os enumerated. So f dataset starts at 
+                            1-1-2001 00:00, hour 00:00 is 0, 01:00 is 1, 02:00 
+                            is 2, etc.
+                    (float) Temperature for that hour. If many are present in 
+                            the dataset, start by using the average.
+
+
+        Returns:    A single value prediction of the load.
+    '''
+    history = [[] for i in range(ridx.logSize)]
+    def __init__(self, inSize = 2, loss = nn.MSELoss()): 
+        super(ANNLFS, self).__init__() 
+        self.firstPass = 1
+        self.linear = nn.Linear(inSize, 1)  # 10 nodes are specified in the thesis.
+        self.loss = loss
+  
+
+    def forward(self, x): 
+        x = F.relu(self.linear(x)) 
+        return x 
+
+    def get_model_descr(self):
+        """Creates a string description of a polynomial."""
+        model = 'y = '
+        modelSize =len(self.linear.weight.data.view(-1)) 
+        for i, w in enumerate(self.linear.weight.data.view(-1)):
+                model += '{}{:.2f} x{} '.format(sign(w), w, modelSize - i)
+        model += '{:+.2f}'.format(self.linear.bias.data[0])
+        return model   
+
+    def shape_input(self, x, label):
+        '''
+            Shape unput data x to: 
+                0-23 : Hourly load of last day
+                24-27: Max and min temp of 2 weather station
+                30-33: Bit encoding for day
+        '''
+
+
+
+        if self.firstPass == 1:
+            self.lastday = x[0:23,:].flatten()
+            self.firstPass = 0
+
+        
+        x = torch.cat((label, x[2:8], x[11:12]), 0)
+        day = x[1]
+
+        label = torch.cat((self.firstDayLabel, llabel[1:] ), 0)
+        # self.firstDayLabel = 
+        return x
+
+    def save_history(self, filePath):
+        trainer.save_log(filePath, self.history)
+
+    def train(self, args, device, trainLoader, testLoader, optim, lossFunction = nn.MSELoss()):
+        true = 0
+        acc  = 0
+        epochs = args[0]
+        
+        trainerArgs = args
+        testerArgs = args
+        testerArgs[1] *= 4 
+        # column_select = torch.tensor([i for i in range(5,30)])
+        for e in range(epochs):
+           trainerArgs[0] = e 
+           testerArgs[0] = e 
+           trainer.train_regressor(self, args, device, trainLoader, optim, lossFunction)
+           self.test(testerArgs, device, testLoader, lossFunction)
+    
+    # Testing and error reports are done here
+    def test(self, args, device, testLoader, lossFunction = nn.MSELoss()):
+        print("--Epoch {} Testing ----" . format(args[0]))        
+        testArgs = args
+        trainer.test_regressor(self, args, device, testLoader, lossFunction) 
+
+
+    def report(self):
+
+        print("Current stats of ANNSLF:")
+        print("MAE:           {}" .format(self.history[ridx.trainMAE]))
+        print("MAPE:          {}" .format(self.history[ridx.trainMAPE]))
+        print("Training Loss: {}" .format(self.history[ridx.trainLoss]))
+        print("Test MAE:      {}" .format(self.history[ridx.testMAE]))
+        print("Test MAPE:     {}" .format(self.history[ridx.testMAE]))
+        print("Test Loss:     {}" .format(self.history[ridx.testLoss]))
+
+
+
