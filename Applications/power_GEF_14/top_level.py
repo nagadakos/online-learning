Fixed the numerical error casuing nan. Was Division by 0. Added e = 0.001

Applications/power_GEF_14/top_level.py
@@ -91,15 +91,15 @@ def main():
     # lines
 
     # Variable Definitions
-    epochs = 30 
+    epochs = 30
     batchSize = 1000
     quantiles = [0.9]
     device  = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # Loss Function Declaration and parameter definitions go here.
     # quantiles = [0.01*i for i in range(1,100)]
-    loss = trainer.QuantileLoss(quantiles)
-    # loss = nn.MSELoss()
+    # loss = trainer.QuantileLoss(quantiles)
+    loss = nn.MSELoss()
     # ---|
 
     # Pass this dictionary as arg input to the init function. The data ranges should be relevant
@@ -134,7 +134,7 @@ def main():
     # ---|
 
     # Report saving and printouts go here
-    print("Learned model:\n" + model.get_model_descr())
+    # print("Learned model:\n" + model.get_model_descr())
     print("Training history:")
     print(model.history)
     model.save_history(logSavePath)

Code/Architecture/ann_greek.py
@@ -50,7 +50,7 @@ class ANNGreek(nn.Module):
         super(ANNGreek, self).__init__() 
         self.firstPass = 1
         self.linear = nn.Linear(inSize, 24)  # 10 nodes are specified in the thesis.
-        self.linear2 = nn.Linear(24, 1)  # 10 nodes are specified in the thesis.
+        self.linear2 = nn.Linear(24, outSize)  # 10 nodes are specified in the thesis.
         self.loss = loss
         self.descr = "ANNGreek" 
         # The list below holds any plotted figure of the model
@@ -97,8 +97,8 @@ class ANNGreek(nn.Module):
 
         epochs = args[0]
         
-        trainerArgs = args
-        testerArgs = args
+        trainerArgs = args.copy()
+        testerArgs = args.copy()
         testerArgs[1] *= 4 
 
         for e in range(epochs):

Code/Datasets/GEF_Power.py
@@ -101,7 +101,8 @@ class GefPower(Dataset):
         # Reshaped data had the label, always at the last column.
         # Raw data has the labels at 3 column.
         if self.toShape is not None:
-            labels = np.asarray(data.iloc[self.lowerBnd:self.upperBnd, -1]) # Reshaped data has last column as
+            # labels = np.asarray(data.iloc[self.lowerBnd:self.upperBnd, -1]) # Reshaped data has last column as
+            labels = data.iloc[self.lowerBnd:self.upperBnd, -1] # Reshaped data has last column as
             data   = data.iloc[self.lowerBnd:self.upperBnd,0:-1] # Last line is label, take it off our data structure
         else:
             labels  = np.asarray(data.iloc[self.lowerBnd:self.upperBnd, 2]) # third column has the load values
@@ -164,7 +165,7 @@ class GefPower(Dataset):
         # if the reshaped data file is needed.
         else:
             itemData = np.array(self.data.iloc[index,:], dtype = float)
-            itemLabel = np.array(self.labels[index], dtype = float)
+            itemLabel = np.array(self.labels.iloc[index], dtype = float)
                     # Turn numpy arrays to Tensors for pytorch usage.
         itemData = torch.from_numpy(itemData).float()
         itemLabel = torch.from_numpy(itemLabel).float()
@@ -429,7 +430,7 @@ def get_files_from_path(targetPath, expression):
 if __name__ == "__main__":
 
     print(" GEF Power Dataset as Main ")
-    myDataset = GefPower('../../Data/GEF/Load', toShape = "GLMLF-C2", transform = "normalize",
+    myDataset = GefPower( toShape = "ANNGreek", transform = "normalize",
                          dataRange=[0, 76799])
 
     myDataset2 = GefPower(task = "Task 1", transform = "normalize",
@@ -437,9 +438,9 @@ if __name__ == "__main__":
 
     # print(myDataset)
     # myDataset.get_data_descr()
-    # print(myDataset.__getitem__(1))
+    print(myDataset.__getitem__(1))
     # print(myDataset.__len__())
-    print(myDataset2.__len__())
-    item, label  = myDataset2.__getitem__(3)
-    print(item, label)
+    # print(myDataset2.__len__())
+    # item, label  = myDataset2.__getitem__(3)
+    # print(item, label)
     print("Size of an instance {}".format(myDataset.get_item_size()))

Code/Tools/trainer.py
@@ -26,6 +26,7 @@ class QuantileLoss(nn.Module):
             self.q = q
         else:
             self.q = [q]
+        self.descr = "QuantileLoss_" + str(len(self.q))
 
     def forward(self, x, target):
         loss = [] # place holder for each quantile loss.
@@ -91,7 +92,7 @@ def train_regressor(model, args, device, indata, optim, lossFunction = nn.MSELos
     '''
     MAE   = 0
     MAPE  = 0
-
+    e = 0.001
     for idx, (data, label) in enumerate(indata):
         data, label = data.to(device), label.to(device)
         # forward pass calculate output of model
@@ -110,7 +111,8 @@ def train_regressor(model, args, device, indata, optim, lossFunction = nn.MSELos
         else:
             loss = lossFunction.forward(pred, label)
         MAE  += torch.FloatTensor.abs(pred.sub(label)).sum().item()
-        MAPE += torch.FloatTensor.abs(pred.sub(label)).div(label).mul(100).sum().item()
+        MAPE += torch.FloatTensor.abs(pred.sub(label)).div(label+e).sum().item()
+
         # Backpropagation part
         # 1. Zero out Grads
         optim.zero_grad()
@@ -123,12 +125,11 @@ def train_regressor(model, args, device, indata, optim, lossFunction = nn.MSELos
             print("Epoch: {}-> Batch: {} / {}, Size: {}. Loss = {}".format(args, idx, len(indata),
                                                                            pred.shape[0], loss.item() ))
             factor = (idx+1)*pred.shape[0]
-            print("Average MAE: {}, Average MAPE: {:.4f}%".format(MAE / factor, MAPE /factor))
-                
+            print("Average MAE: {}, Average MAPE: {:.4f}%".format(MAE / factor, MAPE*100 /factor))
 
     # Log the current train loss
     MAE  = MAE/len(indata.dataset)
-    MAPE = MAPE/len(indata.dataset)
+    MAPE = MAPE*100/len(indata.dataset)
     model.history[ridx.trainLoss].append(loss.item())   #get only the loss value
     model.history[ridx.trainMAE].append(MAE)
     model.history[ridx.trainMAPE].append(MAPE)