22b1b8a70ab8dab0e740736101a08b6c2f2363de
Nikolaos Agadakos <nagadakos@gmail,com>
2018-11-06 11:20:24 -0500

    Updated plotter.main to plot the results of First 200 folder

--- Code/Tools/plotter.py
+++ Code/Tools/plotter.py
@@ -152,14 +152,14 @@ def plot_all_in_one(reps, epochs, title):
 def main():
     title = 'Multi-Linear Regression MSE Loss vs Epoch plot'
     # filePath = "../../Applications/power_GEF_14/Logs/log1.txt"
-    filePath = "../../Applications/power_GEF_14/Logs/ANNGreek"
+    filePath = "../../Applications/power_GEF_14/Logs/ANNGreek/First_logs_200_epochs"
 
     f = get_files_from_path(filePath, "*log1.txt")
     # print(f)
     files = []
     for i in f['files']:
         files.append(join(filePath, i)) 
-    # print(files)
+    print(files)
     plot_regressor(files, 1, title)
     plt.savefig("../../Applications/power_GEF_14/Plots/test.png")
     plt.close()
